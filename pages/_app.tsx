28544f2c2d5836d8bf70a135c27047c8f9f9bfb4
justintemps <jesmith627@gmail.com>
2022-02-02 14:45:47 +0100

    Turn off useLayoutEffect warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { AppProps } from "next/dist/shared/lib/router/router";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../styles/globals";
@@ -5,6 +6,12 @@ import { theme } from "../styles/theme";
 import "../styles/fonts";
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Turn off browser warning about using useLayoutEffect on the server
+  // More info: https://gist.github.com/gaearon/e7d97cdf38a2907924ea12e4ebdf3c85
+  if (!process.browser) {
+    React.useLayoutEffect = React.useEffect;
+  }
+
   return (
     <>
       <GlobalStyle />
