b00a7cdb70454c10d02fb3afc7f3aa29c6160994
justintemps <jesmith627@gmail.com>
2024-06-30 17:23:37 +0200

    Main page hero title

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Justin time to blow your mind</h1>
+
+<style lang="scss">
+  h1 {
+    font-size: rem(136px);
+    line-height: rem(148px);
+    font-weight: bold;
+    filter: drop-shadow(14px 13px 11px rgba(0, 0, 0, 0.5));
+    max-width: calc(var(--size--content--max) - 200px);
+  }
+</style>
