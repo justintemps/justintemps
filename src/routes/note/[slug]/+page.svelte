dacc782b2134e057e8749ca12f4aeef7a7938070
justintemps <jesmith627@gmail.com>
2025-04-08 23:11:17 +0200

    Mobile style fixes

--- src/routes/note/[slug]/+page.svelte
+++ src/routes/note/[slug]/+page.svelte
@@ -25,20 +25,44 @@
 
 <style lang="scss">
   @use "$styles/functions" as *;
+  @use "$styles/mixins" as *;
+
   article {
     gap: 2rem;
-    hgroup {
-      h1 {
-        font-size: px-to-rem(60px);
-        line-height: normal;
-        letter-spacing: -1%;
-      }
 
-      p {
-        font-family: var(--font--display);
+    :global(h2) {
+      font-size: px-to-rem(20px);
+      color: var(--color--brand);
+    }
+  }
+
+  h1 {
+    font-size: px-to-rem(32px);
+    line-height: 1.2;
+    letter-spacing: -1%;
+  }
+
+  p {
+    font-family: var(--font--display);
+    font-size: px-to-rem(20px);
+    line-height: 1.5;
+  }
+
+  @include breakpoint(md) {
+    h1 {
+      font-size: px-to-rem(60px);
+      line-height: 1.2;
+    }
+
+    article {
+      :global(h2) {
         font-size: px-to-rem(24px);
-        line-height: normal;
       }
+    }
+
+    p {
+      font-size: px-to-rem(24px);
+      line-height: 1.5;
     }
   }
 </style>
